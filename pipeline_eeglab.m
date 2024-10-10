f3625bdccda230bd391c6891b681831ac8ba1ef4
st-ts <stetsenko.an@gmail.com>
2024-10-10 17:55:47 -0400

    add the pipeline script

---
+++ pipeline_eeglab.m
@@ -0,0 +1,8 @@
+% Pipeline for the data pre-processing
+% Including:
+% 1. Loading
+% 2. Bandpass filter
+% 3. Artifact correction / rejection
+% 3.1. Eye blinks
+% 3.2. Movements 
+% 4. Convert to cvs or json for further analysis in Python
