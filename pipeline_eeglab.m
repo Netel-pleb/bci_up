d4d9902803bd44a318b36e5e9d75c5a7a6ce21bf
st-ts <stetsenko.an@gmail.com>
2024-10-10 18:33:51 -0400

    add the pipeline script

--- pipeline_eeglab.m
+++ pipeline_eeglab.m
@@ -2,6 +2,7 @@
 % Including:
 % 1. Loading
 % 2. Bandpass filter
+% 3. Re-reference to the ref channel X3
 % 3. Artifact correction / rejection
 % 3.1. Eye blinks
 % 3.2. Movements 
@@ -9,4 +10,15 @@
 
 % Load eeglab set from the one created earlier
 eeglab;
-%load('SubjectJ_LRHand.set')
+data_folder = 'D:\BCI\large_eeg_19ch_13subj';
+set_name = 'SubjectJ_LRHand.set'; % need to make universal later
+EEG = pop_loadset('filename', set_name, 'filepath', data_folder);
+
+% Bandpass filtering (e.g., between 1 and 50 Hz)
+EEG = pop_eegfiltnew(EEG, 'locutoff', 1, 'hicutoff', 50);  % 1-40 Hz bandpass filter
+
+% Re-reference the data to the channel
+EEG = pop_reref(EEG, find(strcmp({EEG.chanlocs.labels}, 'X3')));  % Use [] for average reference
+
+% Run Independent Component Analysis (ICA) to correct for artifacts
+EEG = pop_runica(EEG, 'extended', 1);  % Extended ICA
